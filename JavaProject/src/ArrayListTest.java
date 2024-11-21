bbeb7e07d135faf26b5654efcf6ac1e1be7cab8b
xksdn <xksdn7623@gmail.com>
2024-11-21 22:27:16 +0900

    change name

--- JavaProject/src/ArrayListTest.java
+++ JavaProject/src/ArrayListTest.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class ArrayListTest {
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+        ArrayList<Object> list = new ArrayList<>();
+
+        list.add("apple");
+        list.add(20);
+
+        String name = (String)list.get(0);
+        int count = (int) list.get(1);
+        System.out.println(name + ", " + count);
+    }
 }
