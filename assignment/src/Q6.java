d90873e44706e6c36059fb05c5a44a9f36c5d781
xksdn <xksdn7623@gmail.com>
2024-11-22 21:10:54 +0900

    Q6

--- assignment/src/Q6.java
+++ assignment/src/Q6.java
@@ -1,11 +1,17 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Q6 {
     public static void main(String[] args) {
         List<String> id_list = new ArrayList<>();
         List<String> report = new ArrayList<>();
+
+//
+//        List<String> id_list = new ArrayList<>(Arrays.asList("muzi","frodo","apeach","neo"));
+//        List<String> report = new ArrayList<>(Arrays.asList("muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"));
+//        int k = 2;
+//
+//        System.out.println(solution1(id_list, report, k));
+
 
         Scanner scanner = new Scanner(System.in);
 
@@ -77,8 +83,49 @@ public class Q6 {
 
     }
 
-    public static void solution(List<String> id_list, List<String> report, int k) {
+    public static List<Integer> solution(List<String> id_list, List<String> report, int k) {
+        List<Integer> mail_result = new ArrayList<>();
+        Map<String, Integer> mail_count = new HashMap<>();
 
 
+        // 신고한 사람이 누구를 신고했는지 set을 사용해 중복 제거하여 map에 담기
+        Map<String, Set<String>> report_names_map = new HashMap<>();
+        for (String name : report) { // report 리스트를 map으로 변환
+            String[] names = name.split(" ");
+            String reporter = names[0];
+            String reported = names[1];
+
+            // 키가 이미 존재하는지 확인하고 저장
+            // putIfAbsent: 키가 존재하지 않을 때만 값을 추가
+            report_names_map.putIfAbsent(reporter, new HashSet<>());
+            report_names_map.get(reporter).add(reported);
+        }
+
+        // 신고당한 사람이 몇번 신고 당했는지 카운트
+        for (Set<String> i : report_names_map.values()) {
+            for (String j : i) {
+                mail_count.put(j, mail_count.getOrDefault(j, 0) + 1);
+            }
+        }
+
+        // id에 맞는 mail보내야하는 양을 정함
+        for (String id : id_list) {
+            int count = 0; // 신고 횟수
+
+            for (String name : report) {
+                String[] names = name.split(" ");
+                String reporter = names[0];
+                String reported = names[1];
+
+                // 신고한 아이디와 같고 신고받은 사람의 신고 횟수가 k 이상이면 카운트 증가
+                if (reporter.equals(id) && mail_count.getOrDefault(reported, 0) >= k) {
+                    count++;
+                }
+            }
+            // 메일 추가
+            mail_result.add(count);
+        }
+
+        return mail_result;
     }
 }
